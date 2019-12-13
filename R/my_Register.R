5c11143be76e8da08df72556e84f367fbb49c463
Larry Zhang <lzhang@wprdrstudio01.stats.govt.nz>
2019-12-13 15:54:25 +1300

    write my_Register

---
+++ R/my_Register.R
@@ -0,0 +1,23 @@
+#' Get all R files under a folder
+#'
+#' This returns a dataframe, which has all the R-file information under the specified folder
+#' @param folder_name name of the folder
+#' @export
+#' @author Lingyun (Larry) Zhang \email{lyzhang10@gmail.com}
+#' @importFrom magrittr %>%
+#' @examples
+#' df <- my_Register()
+
+
+my_Register <- function(folder_name = "R")
+{the_folder <- paste0(getwd(), "/", folder_name)
+ file_names <- list.files(path = the_folder, pattern = "R$",
+                          full.names = FALSE)
+ a_df <-
+   data.frame(file_name = file_names) %>%
+   dplyr::mutate(prefix = sub("_.*", "", file_name)) %>%
+   dplyr::mutate(group_letter = substring(prefix, 1, 1)) %>%
+   dplyr::arrange(group_letter, prefix)
+
+ return(a_df)
+}
